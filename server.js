20cdc7d106ca12e79778d96489e66c05bd202ca4
Joe Sullivan <jrs2ea@virginia.edu>
2011-11-08 14:13:03 -0500

    simplified server

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ var io = io.listen(app);
 
 app.listen(3000);
 
+var TestStore = require('./lib/testStore');
+var testStore = TestStore();
+
 io.sockets.on('connection', function (socket) {
-	//catch 'sync' requests
-	var backSocket = require('./lib/backSocket');
-	backSocket(socket);
+	socket.on('sync', function(method,model,cb) {
+		testStore.syncInterface(method,model,function(resp) {
+			cb(resp);
+		});
+	});
 });
